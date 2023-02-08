3a51b2a0deba0a1ff44fa24e0bbc8b4d87c41903
Reza <mrneyestani@gmail.com>
2023-02-08 16:25:41 +0100

    Add JS,HTML,CSS pour la recherche cocktail

--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
+import { searchForCocktails } from "./search";
 import { displaySearchBar } from "./splash";
 
 // Attache un évenement lors du clique sur le splash !
 document
   .querySelector(".splash-container")
   .addEventListener("click", displaySearchBar);
+
+// On attache un évenement lors du clique sur le bouton rechercher
+document
+  .querySelector(".search-bar button")
+  .addEventListener("click", searchForCocktails);
+
+// On ajoute un event sur le keypress de l'input
+document.querySelector("#search").addEventListener("keyup", searchForCocktails);
